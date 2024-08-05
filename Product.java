f94dfc6e58278b35f9b3294aec6a30eb79380a5b
jhh0830 <63404597+jhh0830@users.noreply.github.com>
2024-08-05 17:22:28 +0900

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,20 @@
+package com.hw1.model.vo;
+
+public class Product {
+	private String productId;
+	private String productName;
+	private String puroutcArea;
+	private int price;
+	private double tax;
+	
+	
+	
+	
+public void product(String productId, String productName, String puroutcArea, int price, double tax) {
+		
+		
+	this.price = price;
+		
+		
+	}
+}
